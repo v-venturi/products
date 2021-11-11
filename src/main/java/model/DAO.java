86fa833fbb61267767414b98adcaed6d8904deb5
Vitorio Venturi <v.venturi@yahoo.com.br>
2021-11-11 13:16:27 -0300

    criada conexao mysql

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,35 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
 public class DAO {
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/db_estoque?useTimezone=true&Timezone=UTC";
+	private String user = "root";
+	private String password = "root";
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+		System.out.println(e);
+		return null;			
+		}	
+	}
+	
+	public void testarConexao() {
+	  try {Connection con = conectar();
+	 System.out.println("sucesso!  " + con);
+	 con.close();
+	} catch (Exception e) {
+		System.out.println("erro de conexão  " + e);
+	}
+		
+	}
 
 }
--- src/main/java/model/JavaBeans.java
+++ src/main/java/model/JavaBeans.java
@@ -1,5 +1,48 @@
 package model;
 
 public class JavaBeans {
+	
+	private int id;
+	private String descricao;
+	private double preco;
+	private int estoque;
+	
+	public JavaBeans() {
+		super();
+		
+	}
+	
+	public JavaBeans(int id, String descricao, double preco, int estoque) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.estoque = estoque;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public int getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
 
 }
